c6744d462bd12a40b136f9fefacc751cf1e45107
PradeepKumar <PradeepKumarRebbavarapu705@gmail.com>
2022-05-10 20:17:23 +0530

    i

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}"],
+  "./Components/**/*.{js,ts,jsx,tsx}"],
   important:"#__next",
   theme: {
     extend: {},
